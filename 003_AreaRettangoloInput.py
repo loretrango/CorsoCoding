5d4c3dce34f61d52915b4aa7d0efc9f43b342746
lorenzo <lorenzo.antiga@gmail.com>
2024-12-17 18:03:17 +0100

    commento

--- 003_AreaRettangoloInput.py
+++ 003_AreaRettangoloInput.py
@@ -3,9 +3,11 @@
 print("Area rettangolo")
 print()  # lascia una riga vuota
 
-# accetta un input (che è sempre una stringa (str)), 
-# lo converte in un numero con la virgola (float) 
-# e lo assegna alla variabile
+''' 
+Accetta un input (che è sempre una stringa (str)), 
+lo converte in un numero con la virgola (float) 
+e lo assegna alla variabile
+'''
 base = float(input("Scrivi la base: "))
 altezza = float(input("Scrivi l'altezza: "))
 print()  # lascia una riga vuota
@@ -19,7 +21,7 @@ print("Base:", base)
 print("Altezza:", altezza)
 print("Area:", area)
 
-# approssima il numero a 3 cifre decimale, 
+# Approssima il numero a 3 cifre decimale, 
 # e sovrascrive la variabile area
 area = round(area, 3)
 print("Area (approssimato):", area)
