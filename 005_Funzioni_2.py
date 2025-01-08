e734a8a3235b8bb6fddbe6be1fc8daeb65390d02
lorenzo <lorenzo.antiga@gmail.com>
2025-01-08 16:05:40 +0100

    ...

--- 005_Funzioni_2.py
+++ 005_Funzioni_2.py
@@ -1,3 +1,4 @@
+# dice se un numero è positivo, negativo o zero
 def PositivoNegativo():
     n = 0
     n = int(input("Inserisci numero: "))
@@ -14,5 +15,24 @@ def saluta():
     nome = input("Come ti chiami? ")
     print("Ciao", nome)
 
-PositivoNegativo()
-saluta()
+'''
+Scrivi un programma che chieda all'utente di inserire due numeri.
+    • Stampa quale dei due numeri è maggiore.
+    • Se sono uguali, stampa: "I numeri sono uguali."
+
+    Usate int(input("Inserisci numero: ")) 
+'''
+def confronta():
+    n1 = int(input("Inserisci n1: "))
+    n2 = int(input("Inserisci n2: "))
+
+    if n1 > n2:
+        print(n1, "è maggiore di", n2)
+    elif n1 < n2:
+        print(n2, "è maggiore di", n1)
+    elif n1 == n2:
+        print("I due numeri sono uguali")
+
+#PositivoNegativo()
+#saluta()
+confronta()
