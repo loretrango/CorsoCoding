c08e11f766529022abf63737497d0e041eef84d7
loretrango <lorenzo.antiga@gmail.com>
2025-11-14 20:41:45 +0100

    a2025

---
+++ a2025/001_codice.py
@@ -0,0 +1,17 @@
+# esempio di codice
+# come si stampa qualcosa a video
+print("Ciao, mondo!")
+
+# come si sommano due numeri
+a = 5
+b = 7
+somma = a + b
+print("La somma di", a, "e", b, "è", somma) 
+
+# come si fanno tutte le altre operazioni aritmetiche
+differenza = b - a
+prodotto = a * b
+quoziente = b / a
+print("Differenza:", differenza)
+print("Prodotto:", prodotto)
+print("Quoziente:", quoziente)
