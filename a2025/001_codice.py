25a93a9dd61984fa1febf10851418af6df763aa4
loretrango <lorenzo.antiga@gmail.com>
2025-11-14 20:53:50 +0100

    input

--- a2025/001_codice.py
+++ a2025/001_codice.py
@@ -1,28 +1,34 @@
 # esempio di codice
 # come si stampa qualcosa a video
-print("Ciao, mondo!")
+print("Ciao, mondo!") # ciao mondo
 
 # come si sommano due numeri
 a = 5
 b = 7
 somma = a + b
-print("La somma di", a, "e", b, "è", somma) 
+print("La somma di", a, "e", b, "è", somma) # la somma di 5 e 7 è 12
 
 # come si fanno tutte le altre operazioni aritmetiche
 differenza = b - a
 prodotto = a * b
 quoziente = b / a
-print("Differenza:", differenza)
-print("Prodotto:", prodotto)
-print("Quoziente:", quoziente)
+print("Differenza:", differenza) # differenza: 2
+print("Prodotto:", prodotto)  # prodotto: 35
+print("Quoziente:", quoziente)    # quoziente: 1.4
 
 # come si accetta un input dall'utente
 nome = input("Come ti chiami? ")
-print("Ciao", nome)
+print("Ciao", nome) 
 
 # quando si accetta un input con solo la funzione input(), il valore è sempre una stringa
 # per convertire una stringa in un numero intero, si usa la funzione int()
 a = int(input("Inserisci un numero intero: "))
 b = int(input("Inserisci un altro numero intero: "))
 somma = a + b
-print("La somma di", a, "e", b, "è", somma)
+print("La somma di", a, "e", b, "è", somma) # la somma di X e Y è Z
+
+# se vuoi inserire un numero decimale, usa la funzione float()
+x = float(input("Inserisci un numero decimale: "))
+y = float(input("Inserisci un altro numero decimale: "))
+prodotto = x * y
+print("Il prodotto di", x, "e", y, "è", prodotto) # il prodotto di X e Y è Z
