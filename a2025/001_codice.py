b6f8b73f139b4d9bfeaf8b335926302d0233059c
loretrango <lorenzo.antiga@gmail.com>
2025-11-14 20:45:09 +0100

    codice

--- a2025/001_codice.py
+++ a2025/001_codice.py
@@ -15,3 +15,14 @@ quoziente = b / a
 print("Differenza:", differenza)
 print("Prodotto:", prodotto)
 print("Quoziente:", quoziente)
+
+# come si accetta un input dall'utente
+nome = input("Come ti chiami? ")
+print("Ciao", nome)
+
+# quando si accetta un input con solo la funzione input(), il valore è sempre una stringa
+# per convertire una stringa in un numero intero, si usa la funzione int()
+a = int(input("Inserisci un numero intero: "))
+b = int(input("Inserisci un altro numero intero: "))
+somma = a + b
+print("La somma di", a, "e", b, "è", somma)
