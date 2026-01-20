f91da33b86e831e3f9def84eceea338307cdac21
loretrango <lorenzo.antiga@gmail.com>
2026-01-20 17:42:18 +0100

    codice

--- b2023/codice.py
+++ b2023/codice.py
@@ -11,3 +11,26 @@ A2 = r2**2 * pi
 print("A1:", A1)
 print("A2:", A2)
 
+
+## Problema cilindro. Calcolare il volume e la superficie totale di un cilindro
+## dati raggio della base e altezza
+
+a = 5
+b = 3
+h = 10
+
+Ab = a*b
+print("Area base:", Ab)
+
+P = 2*a + 2*b
+print("Perimetro base:", P)
+
+Al = P*h
+print("Area laterale:", Al)
+At = 2*Ab + Al
+print("Area totale:", At)
+
+V = Ab*h
+print("Volume:", V)
+# fine problema cilindro
+
