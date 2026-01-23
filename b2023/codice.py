8e2706aab500ba4c509ede8ac2ba3049c8611d9f
loretrango <lorenzo.antiga@gmail.com>
2026-01-23 20:41:45 +0100

    acquario

--- b2023/codice.py
+++ b2023/codice.py
@@ -33,3 +33,39 @@ V = Ab*h
 print("Volume:", V)
 # fine problema cilindro
 
+print("-----")
+## Problema acquario a forma di parallelepipedo, misure in cm, a=45 x b=41 x h=105
+# prezzo vetro 40euro/m2, calcolare il costo del vetro necessario per costruire l'acquario
+# il vetro si trova su tutte le superfici compresa la base e il coperchio
+print("Acquario")
+
+# dati
+print("Dati:")
+a = 45
+h = 41
+b = 105
+costo_vetro_per_m2 = 40  # euro
+print("Costo vetro per m2 (euro):", costo_vetro_per_m2)
+print("Dimensioni acquario (cm):", a, "x", b, "x", h)
+
+# svolgimento
+print("Svolgimento:")
+Ab = a*b
+print("Area base:", Ab)
+
+P = 2*a + 2*b
+print("Perimetro base:", P)
+
+Al = P*h
+print("Area laterale:", Al)
+At = 2*Ab + Al
+print("Area totale:", At)
+
+V = Ab*h
+print("Volume:", V)
+
+At_m2 = At / 10000  # convertire cm2 in m2
+costo_totale_vetro = At_m2 * costo_vetro_per_m2
+print("Costo totale vetro (euro):", costo_totale_vetro)
+# fine problema acquario
+
